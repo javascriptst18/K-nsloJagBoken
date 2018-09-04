7661b5ea9b4a05019d3d69df4969974e3650fc4e
agnekil <nathalie@agnekil.com>
2018-09-04 10:53:41 +0200

    suck

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import StartScreen from './components/StartScreen';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit 
-{' '}
-<code>src/App.js</code>
-{' '}
-and save to reload.
-</p>
-=======
-        <StartScreen />
->>>>>>> 254cd0fdd1fec0f6110c3066b8f63095874e5726
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
       </div>
     );
   }
