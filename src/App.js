3fe9e25356843d7a17e7349e5aad48c78d3f40ef
agnekil <nathalie@agnekil.com>
2018-09-04 10:46:51 +0200

    before pull

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
+          To get started, edit 
+{' '}
+<code>src/App.js</code>
+{' '}
 and save to reload.
-        </p>
+</p>
       </div>
     );
   }
